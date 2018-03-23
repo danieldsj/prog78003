41a138615bf6bd53aaff2ba90583bd5c7c3c9ef4
Daniel de Sao Jose <danieldsj@gmail.com>
2018-03-22 20:47:07 -0400

    Added comments to exercise 3.

--- src/DanielExercise03.java
+++ src/DanielExercise03.java
@@ -7,21 +7,30 @@ Requierments:
 * Display ten characters per line.
 * Characters are separated by exactly one space.
 
-
  */
 public class DanielExercise03 {
     public static void main(String[] args) {
+        
+        // Create a character to count the columns.
         int col = 0;
         
         for(int c = (int)'!'; c <= (int)'~'; c++) {
-            System.out.printf("%2s", (char)c);
+            
+            // Print the character with a leading space.
+            System.out.printf("%2s", (char)c); 
+            
+            // Increment the column value.
             col++;
+            
             if (col > 9) {
-                col = 0;
+                // Reset the colulmn variable to 0 when we reach the end.
+                col = 0; 
+                // Print a new line.
                 System.out.println();
             }
         }
         
+        // New line at the end.
         System.out.println();
     }
 }
